e5af43e61fbf80a784dfa807ea652c727c742d3e
Gayatri Parripati <your-github-email@example.com>
2026-07-18 10:18:19 +0530

    add datatypes exp

---
+++ datatypes.ts
@@ -0,0 +1,13 @@
+let studentName: string = "Manikanta";
+let age: number = 20;
+let marks: number = 95.5;
+let college: string = "XYZ College";
+let isStudent: boolean = true;
+let passed: boolean = false;
+
+console.log(studentName);
+console.log(age);
+console.log(marks);
+console.log(college);
+console.log(isStudent);
+console.log(passed);
